cc7244908fcbd9e8a17f5ea008b9bcccd34b8884
Bert Belder <bertbelder@gmail.com>
2010-06-30 00:45:12 +0200

    Use generic types for NullFilter instances

--- src/java/de/incunabulum/owl2java/core/model/jenautils/NullFilter.java
+++ src/java/de/incunabulum/owl2java/core/model/jenautils/NullFilter.java
@@ -2,10 +2,10 @@ package de.incunabulum.owl2java.core.model.jenautils;
 
 import com.hp.hpl.jena.util.iterator.Filter;
 
-public class NullFilter extends Filter {
+public class NullFilter<T> extends Filter<T> {
 
 	@Override
-	public boolean accept(Object obj) {
+	public boolean accept(T obj) {
 		return (obj == null);
 	}
 
