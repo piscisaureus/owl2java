fca4945aeabad3b8b4142dbe0ff1c009a14bb320
Bert Belder <bertbelder@gmail.com>
2010-07-01 16:37:50 +0200

    Remove all db4o generation code

--- src/java/de/incunabulum/owl2java/core/test/TestGenerators.java
+++ src/java/de/incunabulum/owl2java/core/test/TestGenerators.java
@@ -7,7 +7,6 @@ import junit.framework.TestCase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import de.incunabulum.owl2java.core.Db4oGenerator;
 import de.incunabulum.owl2java.core.JenaGenerator;
 
 public class TestGenerators extends TestCase{
@@ -53,39 +52,4 @@ public class TestGenerators extends TestCase{
 		long elapse = stopDate.getTime() - startDate.getTime();
 		log.info("Test finished (" + elapse + " ms)");
 	}
-
-	public static void testDb4oGenerator() {
-		Date startDate = new Date();
-
-		Db4oGenerator gen = new Db4oGenerator();
-		// gen.setGenerateMergeCode(true);
-		// gen.setGenerationType(Db4oGenerator.ClassBasedGeneration);
-		// gen.setInstanceClassName("MergeCode");
-
-		String uri = "";
-
-		// simple code, not using multiple inheritance
-		uri = "http://owl.incunabulum.de/2008-Jakuzi/owl4java-simple.owl";
-		gen.generate(uri, "src/testOut", "db4otest.classbased");
-
-		uri = "http://owl.incunabulum.de/2008-Base/cutoutsClips.owl";
-		gen.generate(uri, "src/testOut", "db4otest.cutoutsclips");
-
-		// report
-		String report = gen.getJModelReport();
-		// log.error(report);
-
-		report = gen.getStatistics();
-		log.error(report);
-		log.error(gen.getJModelReport());
-
-		Date stopDate = new Date();
-		long elapse = stopDate.getTime() - startDate.getTime();
-		log.info("Test finished (" + elapse + " ms)");
-
-	}
-
-	
-
-
 }
