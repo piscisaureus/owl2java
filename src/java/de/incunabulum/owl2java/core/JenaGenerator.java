aa70d8bbbb9019fbde9d7077c79a97f56a6fae71
Bert Belder <bertbelder@gmail.com>
2010-07-01 17:17:02 +0200

    Merge AbstractGenerator into JenaGenerator and AbstractWriter into JenaWriter

--- src/java/de/incunabulum/owl2java/core/JenaGenerator.java
+++ src/java/de/incunabulum/owl2java/core/JenaGenerator.java
@@ -1,6 +1,10 @@
 package de.incunabulum.owl2java.core;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,8 +17,9 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import de.incunabulum.owl2java.core.generator.ModelPreparation;
 import de.incunabulum.owl2java.core.generator.OwlReader;
 import de.incunabulum.owl2java.core.generator.jena.JenaWriter;
+import de.incunabulum.owl2java.core.model.jmodel.JModel;
 
-public class JenaGenerator extends AbstractGenerator {
+public class JenaGenerator {
 
 	@SuppressWarnings("unused")
 	private static Log log = LogFactory.getLog(JenaGenerator.class);
@@ -25,6 +30,30 @@ public class JenaGenerator extends AbstractGenerator {
 	private String testClassName = "Test";
 
 	private boolean createTestClass = true;
+
+	String toolsPackage = "tools";
+
+	Date startAll;
+
+	Date startJModedlCreation;
+
+	Date startPreparation;
+
+	Date startToDisk;
+
+	Date stopAll;
+
+	OntModel model;
+
+	private Map<String, String> mappings = new HashMap<String, String>();
+
+	JModel jmodel;
+
+	protected boolean reasignDomainlessProperties = true;
+
+	protected boolean enableCodeFormatting = true;
+
+	protected Properties codeFormatterOptions = null;
 
 	public void generate(String uri, String altLocation, String baseDir, String basePackage) {
 		startAll = new Date();
@@ -100,6 +129,79 @@ public class JenaGenerator extends AbstractGenerator {
 
 	public void setTestcaseName(String testcaseName) {
 		this.testClassName = testcaseName;
+	}
+
+	public boolean addMappings(String uri, String altLocation) {
+		if (uri == null || altLocation == null) {
+			return false;
+		}
+		if (mappings == null) {
+			mappings = new HashMap<String, String>();
+		}
+		if (mappings.containsKey(uri)) {
+			return false;
+		}
+		mappings.put(uri, altLocation);
+		return true;
+	}
+
+	protected void addMappings(OntDocumentManager owlDocMgr, String top_uri) {
+		if (mappings != null && !mappings.isEmpty()) {
+			int lastSlash = top_uri == null ? 0 : top_uri.lastIndexOf('/');
+			String baseuri = lastSlash >= 0 ? top_uri.substring(0, lastSlash) : top_uri;
+			for (int i = 0; i < mappings.size(); i++) {
+				Iterator<String> itr = mappings.keySet().iterator();
+				while (itr.hasNext()) {
+					String uri = itr.next();
+					String altloc = baseuri + "/" + mappings.get(uri);
+					owlDocMgr.addAltEntry(uri, altloc);
+					System.out.println("uri=" + uri + "; altloc=" + altloc);
+				}
+			}
+		}
+	}
+
+	public String getStatistics() {
+		String str = new String();
+		if (this.jmodel != null)
+			str += jmodel.getStatistics() + "\n";
+		str += "Total Time: " + (stopAll.getTime() - startAll.getTime()) + " ms\n";
+		str += "Reading the Owl Model: " + (startJModedlCreation.getTime() - startAll.getTime()) + " ms\n";
+		str += "Creating the JModel: " + (startPreparation.getTime() - startJModedlCreation.getTime()) + " ms\n";
+		str += "Preparing the JModel: " + (startToDisk.getTime() - startPreparation.getTime()) + " ms\n";
+		str += "Writting the classes: " + (stopAll.getTime() - startToDisk.getTime()) + " ms\n";
+	
+		return str;
+	}
+
+	public String getJModelReport() {
+		if (this.jmodel != null)
+			return jmodel.getJModelReport();
+		return new String();
+	}
+
+	public OntModel getOntModel() {
+		return model;
+	}
+
+	public JModel getJModel() {
+		return jmodel;
+	}
+
+	public void setToolsPackage(String toolsPackage) {
+		this.toolsPackage = toolsPackage;
+	}
+
+	public void setReasignDomainlessProperties(boolean reasignDomainlessProperties) {
+		this.reasignDomainlessProperties = reasignDomainlessProperties;
+	}
+
+	public void setEnableCodeFormating(boolean enableCodeFormatting) {
+		this.enableCodeFormatting = enableCodeFormatting;
+	}
+
+	public void setCodeFormatterOptions(Properties codeFormatterOptions) {
+		this.codeFormatterOptions = codeFormatterOptions;
 	}
 
 }
--- src/java/de/incunabulum/owl2java/core/generator/jena/JenaWriter.java
+++ src/java/de/incunabulum/owl2java/core/generator/jena/JenaWriter.java
@@ -1,20 +1,28 @@
 package de.incunabulum.owl2java.core.generator.jena;
 
+import java.io.File;
+import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Iterator;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 
-import de.incunabulum.owl2java.core.generator.AbstractWriter;
+import de.incunabulum.owl2java.core.formatter.CodeFormattingWriter;
+import de.incunabulum.owl2java.core.generator.AbstractCodeWriterFactory;
+import de.incunabulum.owl2java.core.generator.ICodeWriterFactory;
 import de.incunabulum.owl2java.core.model.jmodel.JClass;
 import de.incunabulum.owl2java.core.model.jmodel.JModel;
+import de.incunabulum.owl2java.core.model.jmodel.JPackage;
 import de.incunabulum.owl2java.core.model.jmodel.utils.NamingUtils;
 import de.incunabulum.owl2java.core.model.xsd.XsdMapTestData;
+import de.incunabulum.owl2java.core.utils.JavaUtils;
 
-public class JenaWriter extends AbstractWriter {
+public class JenaWriter {
 	
 	static Log log = LogFactory.getLog(JenaWriter.class);
 	public static final String templateDirJena = "jenaTemplates"; 
@@ -23,6 +31,13 @@ public class JenaWriter extends AbstractWriter {
 	private String factoryName;
 	private String testClassName;
 	private boolean generateTestClass;
+	protected String baseDir;
+	protected String basePackage;
+	protected String toolsPackage;
+	private boolean enableCodeFormatting;
+	private Properties codeFormatterOptions;
+	protected JModel jmodel;
+	protected VelocityEngine vEngine;
 	
 	public static String getTemplatePath(String templateName) {
 		return "/" + templateDirJena + "/" + templateName;
@@ -144,6 +159,91 @@ public class JenaWriter extends AbstractWriter {
 
 	public void setTestClassName(String testClassName) {
 		this.testClassName = testClassName;
+	}
+
+
+	protected void createPackageDirectories() {
+		boolean success = true;
+	
+		log.info("Creating directory structure");
+		// create base directory
+		log.debug("Creating base directory " + baseDir);
+		success &= new File(baseDir).mkdirs();
+	
+		// dito for all package directories
+		Iterator<JPackage> pkgIt = jmodel.listPackages().iterator();
+		while (pkgIt.hasNext()) {
+			JPackage pkg = pkgIt.next();
+			if (pkg.listJClasses().size() > 0) {
+				String pkgName = pkg.getPackageName();
+				String pkgDir = JavaUtils.toDirectoryFromPackage(pkgName, baseDir);
+				log.debug("Creating directory for package " + pkgName);
+				success &= new File(pkgDir).mkdirs();
+			}
+		}
+		// finally for the tools package
+		String pkgName = NamingUtils.getJavaPackageName(basePackage, toolsPackage);
+		String pkgDir = JavaUtils.toDirectoryFromPackage(pkgName, baseDir);
+		log.debug("Creating tools directory for package " + pkgName);
+		success &= new File(pkgDir).mkdirs();
+	}
+
+
+	protected void initVelocityEngine(String baseDir) {
+		log.info("Init velocity engine");
+	
+		vEngine = new VelocityEngine();
+	
+		vEngine.setProperty("resource.loader", "class");
+		vEngine.setProperty("class.resource.loader.description",
+				"Velocity Classpath Resource Loasder");
+		vEngine.setProperty("class.resource.loader.class",
+				"org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+	
+		// see http://minaret.biz/tips/tomcatLogging.html
+		vEngine.setProperty("runtime.log.logsystem.class",
+				"org.apache.velocity.runtime.log.Log4JLogSystem");
+		vEngine.setProperty("velocimacro.library", "/" + baseDir + "/" + "macros.vm");
+		try {
+			vEngine.init();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+	
+	}
+
+
+	/**
+	 * Creates a factory for stream writers that write source code to a specified file
+	 */
+	protected ICodeWriterFactory getCodeWriterFactory() {
+		return new AbstractCodeWriterFactory() {
+			@Override
+			public Writer getCodeWriter(Writer destination) {
+				if (enableCodeFormatting) {
+					// If source code formatting is enabled, wrap the writer in a source code formatting writer
+					return new CodeFormattingWriter(destination, codeFormatterOptions); 
+				} else {
+					// Otherwise just return the destination writer
+					return destination;
+				}
+			}
+		};
+	}
+
+
+	public void setToolsPackage(String toolsPackage) {
+		this.toolsPackage = toolsPackage;
+	}
+
+
+	public void setEnableCodeFormatting(boolean enableCodeFormatting) {
+		this.enableCodeFormatting = enableCodeFormatting;
+	}
+
+
+	public void setCodeFormatterOptions(Properties codeFormatterOptions) {
+		this.codeFormatterOptions = codeFormatterOptions;
 	}
 
 }
