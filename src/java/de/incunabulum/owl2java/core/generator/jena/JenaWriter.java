e67a51195cb60ca4c87d6ed56ff333182ed26fc9
Bert Belder <bertbelder@gmail.com>
2010-07-01 18:39:06 +0200

    Integrate all Jena class writers into JenaWriter

--- src/java/de/incunabulum/owl2java/core/generator/jena/JenaWriter.java
+++ src/java/de/incunabulum/owl2java/core/generator/jena/JenaWriter.java
@@ -2,15 +2,18 @@ package de.incunabulum.owl2java.core.generator.jena;
 
 import java.io.File;
 import java.io.Writer;
-import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 
 import de.incunabulum.owl2java.core.formatter.CodeFormattingWriter;
 import de.incunabulum.owl2java.core.generator.AbstractCodeWriterFactory;
@@ -23,30 +26,35 @@ import de.incunabulum.owl2java.core.model.xsd.XsdMapTestData;
 import de.incunabulum.owl2java.core.utils.JavaUtils;
 
 public class JenaWriter {
-	
-	static Log log = LogFactory.getLog(JenaWriter.class);
-	public static final String templateDirJena = "jenaTemplates"; 
+	private static final String CLASS_TEMPLATE_NAME = "class.vm";
+	private static final String FACTORY_TEMPLATE_NAME = "factory.vm";
+	private static final String INTERFACE_TEMPLATE_NAME = "interface.vm";
+	private static final String TEST_TEMPLATE_NAME = "test.vm";
+	private static final String VOCABULARY_TEMPLATE_NAME = "vocabulary.vm";
 
-	private String vocabularyName;
-	private String factoryName;
-	private String testClassName;
-	private boolean generateTestClass;
-	protected String baseDir;
-	protected String basePackage;
-	protected String toolsPackage;
-	private boolean enableCodeFormatting;
-	private Properties codeFormatterOptions;
-	protected JModel jmodel;
-	protected VelocityEngine vEngine;
-	
-	public static String getTemplatePath(String templateName) {
+	private static final String templateDirJena = "jenaTemplates";
+
+	static Log log = LogFactory.getLog(JenaWriter.class);
+
+	private static String getTemplatePath(String templateName) {
 		return "/" + templateDirJena + "/" + templateName;
 	}
 
+	private String baseDir;
+	private String basePackage;
+	private Properties codeFormatterOptions;
+	private boolean enableCodeFormatting;
+	private String factoryName;
+	private boolean generateTestClass;
+	private JModel jmodel;
+	private String testClassName;
+	private String toolsPackage;
+	private VelocityEngine vEngine;
+	private String vocabularyName;
 
 	public void generate(JModel model, String baseDir, String basePackage) {
 		this.baseDir = baseDir;
-		this.jmodel = model;
+		jmodel = model;
 		this.basePackage = basePackage;
 
 		log.info("");
@@ -74,102 +82,146 @@ public class JenaWriter {
 		if (generateTestClass) {
 			createTestClass();
 		}
-	}
-
-	protected void createTestClass() {
-		log.info("Creating Testclass");
-		JenaTestWriter tWriter = new JenaTestWriter(vEngine, getBaseVelocityContext(), getCodeWriterFactory());
-		tWriter.setTestClassName(testClassName);
-		tWriter.setToolsPackage(toolsPackage);
-		tWriter.writeTestCases(jmodel, baseDir, basePackage);
-	}
-
-	protected void createFactory() {
-		log.info("Creating Factory");
-		JenaFactoryWriter fWriter = new JenaFactoryWriter(vEngine, getBaseVelocityContext(), getCodeWriterFactory());
-		fWriter.setFactoryName(factoryName);
-		fWriter.setToolsPackage(toolsPackage);
-		fWriter.writeFactory(jmodel, baseDir, basePackage);
-	}
-
-	protected void createVocabulary() {
-		log.info("Creating vocabulary");
-		JenaVocabularyWriter vWriter = new JenaVocabularyWriter(vEngine, getBaseVelocityContext(), getCodeWriterFactory());
-		vWriter.setVocabularyName(vocabularyName);
-		vWriter.setToolsPackage(toolsPackage);
-		vWriter.writeVocabulary(jmodel, baseDir, basePackage);
-	}
-
-
-	protected VelocityContext getBaseVelocityContext() {
-		// add some default stuff to our context. These are reused over all writers
-		VelocityContext vContext = new VelocityContext();
-		Calendar c = Calendar.getInstance();
-		vContext.put("now", SimpleDateFormat.getInstance().format(c.getTime()));
-		vContext.put("pkgBase", basePackage);
-		vContext.put("pkgTools", toolsPackage);
-		vContext.put("jmodel", jmodel);
-		vContext.put("factoryName", factoryName);
-		vContext.put("factoryPkg", NamingUtils.getJavaPackageName(basePackage, toolsPackage));
-		vContext.put("vocabName", vocabularyName);
-		vContext.put("vocabPkg", NamingUtils.getJavaPackageName(basePackage, toolsPackage));
-		vContext.put("testcaseName", testClassName);
-		vContext.put("testcasePkg", NamingUtils.getJavaPackageName(basePackage, toolsPackage));
-		XsdMapTestData xsdMap = new XsdMapTestData();
-		vContext.put("xsdMap", xsdMap);
-		return vContext;
-	}
-
-	protected void createInterfaces() {
-		log.info("Creating java interfaces");
-
-		JenaInterfaceWriter ifaceWriter = new JenaInterfaceWriter(vEngine, getBaseVelocityContext(), getCodeWriterFactory());
-
-		Iterator<JClass> clsIt = jmodel.listJClasses().iterator();
-		while (clsIt.hasNext()) {
-			JClass cls = clsIt.next();
-			ifaceWriter.writeInterface(jmodel, cls, baseDir);
-		}
-	}
-
-	protected void createClasses() {
-		log.info("Creating java classes");
-		JenaClassWriter clsWriter = new JenaClassWriter(vEngine, getBaseVelocityContext(), getCodeWriterFactory());
-
-		Iterator<JClass> clsIt = jmodel.listJClasses().iterator();
-		while (clsIt.hasNext()) {
-			JClass cls = clsIt.next();
-			clsWriter.writeClass(jmodel, cls, baseDir);
-		}
 
 	}
 
-	public void setVocabularyName(String vocabularyName) {
-		this.vocabularyName = vocabularyName;
-	}
-
-
-	public void setFactoryName(String factoryName) {
-		this.factoryName = factoryName;
+	public void setCodeFormatterOptions(Properties codeFormatterOptions) {
+		this.codeFormatterOptions = codeFormatterOptions;
 	}
 
 	public void setCreateTestClass(boolean createTestClass) {
-		this.generateTestClass = createTestClass;
+		generateTestClass = createTestClass;
+	}
+
+	public void setEnableCodeFormatting(boolean enableCodeFormatting) {
+		this.enableCodeFormatting = enableCodeFormatting;
+	}
+
+	public void setFactoryName(String factoryName) {
+		this.factoryName = factoryName;
 	}
 
 	public void setTestClassName(String testClassName) {
 		this.testClassName = testClassName;
 	}
 
+	public void setToolsPackage(String toolsPackage) {
+		this.toolsPackage = toolsPackage;
+	}
 
-	protected void createPackageDirectories() {
+	public void setVocabularyName(String vocabularyName) {
+		this.vocabularyName = vocabularyName;
+	}
+
+	private void createClasses() {
+		log.info("Creating java classes");
+		Iterator<JClass> clsIt = jmodel.listJClasses().iterator();
+		while (clsIt.hasNext()) {
+			JClass cls = clsIt.next();
+			String outDir = JavaUtils.toDirectoryFromPackage(cls.getJavaPackageName(), baseDir);
+			String outName = cls.getJavaClassName();
+			String outPath = outDir + "/" + outName + ".java";
+			log.info("Creating class " + outName);
+			log.debug("Creating class as " + outPath);
+
+			Template template;
+			try {
+				String templatePath = JenaWriter.getTemplatePath(CLASS_TEMPLATE_NAME);
+				template = vEngine.getTemplate(templatePath);
+			} catch (ResourceNotFoundException e) {
+				throw new RuntimeException();
+			} catch (ParseErrorException e) {
+				throw new RuntimeException();
+			} catch (Exception e) {
+				throw new RuntimeException();
+			}
+
+			VelocityContext vContext = getBaseVelocityContext();
+			vContext.put("cls", cls);
+
+			try {
+				Writer writer = getCodeWriterFactory().getCodeWriter(outPath);
+				template.merge(vContext, writer);
+				writer.close();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private void createFactory() {
+		String pkgName = NamingUtils.getJavaPackageName(basePackage, toolsPackage);
+		String outDir = JavaUtils.toDirectoryFromPackage(pkgName, baseDir);
+		String outName = factoryName;
+		String outPath = outDir + "/" + outName + ".java";
+		log.debug("Creating factory " + outPath);
+
+		Template template;
+		try {
+			String templatePath = JenaWriter.getTemplatePath(FACTORY_TEMPLATE_NAME);
+			template = vEngine.getTemplate(templatePath);
+		} catch (ResourceNotFoundException e) {
+			throw new RuntimeException();
+		} catch (ParseErrorException e) {
+			throw new RuntimeException();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+
+		try {
+			Writer writer = getCodeWriterFactory().getCodeWriter(outPath);
+			template.merge(getBaseVelocityContext(), writer);
+			writer.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void createInterfaces() {
+		log.info("Creating java interfaces");
+
+		Iterator<JClass> clsIt = jmodel.listJClasses().iterator();
+		while (clsIt.hasNext()) {
+			JClass cls = clsIt.next();
+			String outDir = JavaUtils.toDirectoryFromPackage(cls.getJavaPackageName(), baseDir);
+			String outName = cls.getJavaInterfaceName();
+			String outPath = outDir + "/" + outName + ".java";
+			log.info("Creating interface " + outName);
+			log.debug("Creating interface as " + outPath);
+
+			Template template;
+			try {
+				String templatePath = JenaWriter.getTemplatePath(INTERFACE_TEMPLATE_NAME);
+				template = vEngine.getTemplate(templatePath);
+			} catch (ResourceNotFoundException e) {
+				throw new RuntimeException();
+			} catch (ParseErrorException e) {
+				throw new RuntimeException();
+			} catch (Exception e) {
+				throw new RuntimeException();
+			}
+
+			VelocityContext vContext = getBaseVelocityContext();
+			vContext.put("cls", cls);
+
+			try {
+				Writer writer = getCodeWriterFactory().getCodeWriter(outPath);
+				template.merge(vContext, writer);
+				writer.close();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private void createPackageDirectories() {
 		boolean success = true;
-	
+
 		log.info("Creating directory structure");
 		// create base directory
 		log.debug("Creating base directory " + baseDir);
 		success &= new File(baseDir).mkdirs();
-	
+
 		// dito for all package directories
 		Iterator<JPackage> pkgIt = jmodel.listPackages().iterator();
 		while (pkgIt.hasNext()) {
@@ -188,41 +240,94 @@ public class JenaWriter {
 		success &= new File(pkgDir).mkdirs();
 	}
 
+	private void createTestClass() {
+		String pkgName = NamingUtils.getJavaPackageName(basePackage, toolsPackage);
+		String outDir = JavaUtils.toDirectoryFromPackage(pkgName, baseDir);
+		String outName = testClassName;
+		String outPath = outDir + "/" + outName + ".java";
+		log.debug("Creating test cases " + outPath);
 
-	protected void initVelocityEngine(String baseDir) {
-		log.info("Init velocity engine");
-	
-		vEngine = new VelocityEngine();
-	
-		vEngine.setProperty("resource.loader", "class");
-		vEngine.setProperty("class.resource.loader.description",
-				"Velocity Classpath Resource Loasder");
-		vEngine.setProperty("class.resource.loader.class",
-				"org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-	
-		// see http://minaret.biz/tips/tomcatLogging.html
-		vEngine.setProperty("runtime.log.logsystem.class",
-				"org.apache.velocity.runtime.log.Log4JLogSystem");
-		vEngine.setProperty("velocimacro.library", "/" + baseDir + "/" + "macros.vm");
+		Template template;
 		try {
-			vEngine.init();
+			String templatePath = JenaWriter.getTemplatePath(TEST_TEMPLATE_NAME);
+			template = vEngine.getTemplate(templatePath);
+		} catch (ResourceNotFoundException e) {
+			throw new RuntimeException();
+		} catch (ParseErrorException e) {
+			throw new RuntimeException();
 		} catch (Exception e) {
 			throw new RuntimeException();
 		}
-	
+
+		try {
+			Writer writer = getCodeWriterFactory().getCodeWriter(outPath);
+			template.merge(getBaseVelocityContext(), writer);
+			writer.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
+	private void createVocabulary() {
+		String pkgName = NamingUtils.getJavaPackageName(basePackage, toolsPackage);
+		String outDir = JavaUtils.toDirectoryFromPackage(pkgName, baseDir);
+		String outName = vocabularyName;
+		String outPath = outDir + "/" + outName + ".java";
+		log.debug("Creating vocabulary " + outPath);
+
+		Template template;
+		try {
+			String templatePath = JenaWriter.getTemplatePath(VOCABULARY_TEMPLATE_NAME);
+			template = vEngine.getTemplate(templatePath);
+		} catch (ResourceNotFoundException e) {
+			throw new RuntimeException();
+		} catch (ParseErrorException e) {
+			throw new RuntimeException();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+
+		try {
+			Writer writer = getCodeWriterFactory().getCodeWriter(outPath);
+			template.merge(getBaseVelocityContext(), writer);
+			writer.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private VelocityContext getBaseVelocityContext() {
+		// add some default stuff to our context. These are reused over all
+		// writers
+		VelocityContext vContext = new VelocityContext();
+		Calendar c = Calendar.getInstance();
+		vContext.put("now", DateFormat.getInstance().format(c.getTime()));
+		vContext.put("pkgBase", basePackage);
+		vContext.put("pkgTools", toolsPackage);
+		vContext.put("jmodel", jmodel);
+		vContext.put("factoryName", factoryName);
+		vContext.put("factoryPkg", NamingUtils.getJavaPackageName(basePackage, toolsPackage));
+		vContext.put("vocabName", vocabularyName);
+		vContext.put("vocabPkg", NamingUtils.getJavaPackageName(basePackage, toolsPackage));
+		vContext.put("testcaseName", testClassName);
+		vContext.put("testcasePkg", NamingUtils.getJavaPackageName(basePackage, toolsPackage));
+		XsdMapTestData xsdMap = new XsdMapTestData();
+		vContext.put("xsdMap", xsdMap);
+		return vContext;
+	}
 
 	/**
-	 * Creates a factory for stream writers that write source code to a specified file
+	 * Creates a factory for stream writers that write source code to a
+	 * specified file
 	 */
-	protected ICodeWriterFactory getCodeWriterFactory() {
+	private ICodeWriterFactory getCodeWriterFactory() {
 		return new AbstractCodeWriterFactory() {
 			@Override
 			public Writer getCodeWriter(Writer destination) {
 				if (enableCodeFormatting) {
-					// If source code formatting is enabled, wrap the writer in a source code formatting writer
-					return new CodeFormattingWriter(destination, codeFormatterOptions); 
+					// If source code formatting is enabled, wrap the writer in
+					// a source code formatting writer
+					return new CodeFormattingWriter(destination, codeFormatterOptions);
 				} else {
 					// Otherwise just return the destination writer
 					return destination;
@@ -231,19 +336,24 @@ public class JenaWriter {
 		};
 	}
 
+	private void initVelocityEngine(String baseDir) {
+		log.info("Init velocity engine");
 
-	public void setToolsPackage(String toolsPackage) {
-		this.toolsPackage = toolsPackage;
-	}
+		vEngine = new VelocityEngine();
 
+		vEngine.setProperty("resource.loader", "class");
+		vEngine.setProperty("class.resource.loader.description", "Velocity Classpath Resource Loasder");
+		vEngine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
 
-	public void setEnableCodeFormatting(boolean enableCodeFormatting) {
-		this.enableCodeFormatting = enableCodeFormatting;
-	}
+		// see http://minaret.biz/tips/tomcatLogging.html
+		vEngine.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.Log4JLogSystem");
+		vEngine.setProperty("velocimacro.library", "/" + baseDir + "/" + "macros.vm");
+		try {
+			vEngine.init();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
 
-
-	public void setCodeFormatterOptions(Properties codeFormatterOptions) {
-		this.codeFormatterOptions = codeFormatterOptions;
 	}
 
 }
